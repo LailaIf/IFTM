cd8912c108e34027927d14b3af3ce783dec43371
Laila Cristina <161842043+LailaIf@users.noreply.github.com>
2026-08-14 15:39:27 -0300

    Add files via upload

---
+++ 2periodo/JS/listas/lista10.js
@@ -0,0 +1,7 @@
+vezes = prompt("Insira um numero maior que zero.");
+nome = prompt("Insira seu nome completo.");
+
+for(let i=0; i<vezes; i++){
+    document.write(nome);
+    document.write(`<br>`);
+}
---
+++ 2periodo/JS/listas/lista9.js
@@ -0,0 +1,3 @@
+expressao = prompt("Escreva uma expressao matematica como ((5*8)-9).");
+
+alert(`Resultado da expressao: ${eval(expressao)}`);
